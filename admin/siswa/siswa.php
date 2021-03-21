be8cc268b49db09f2229bc152d30256e11ea6693
aadiityaak <42263468+aadiityaak@users.noreply.github.com>
2021-03-21 17:38:20 +0700

    Add edit modal

--- admin/siswa/siswa.php
+++ admin/siswa/siswa.php
@@ -22,6 +22,8 @@
 					<th>Saudara (Sekolah Disini)</th>
 					<th>Pendapatan Ortu</th>
 					<th>SPP</th>
+					<th></th>
+					<th></th>
 				</tr>
 			</thead>
 			<tfoot>
@@ -41,9 +43,32 @@
 					<th>Saudara (Sekolah Disini)</th>
 					<th>Pendapatan Ortu</th>
 					<th>SPP</th>
+					<th></th>
+					<th></th>
 				</tr>
 			</tfoot>
 		</table>
 		</div>
 	</div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" style="max-height:70vh;overflow-y:auto;">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary ">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
